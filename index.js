4d399b015eed3e336fa1e804d6163f6f963919e3
Keino Chichester <keino.chichester@gmail.com>
2023-01-03 22:29:57 -0500

    added loan calc

---
+++ index.js
@@ -0,0 +1,9 @@
+function calcLoan() {
+    const amount = document.querySelector("#amount").value;
+    const rate = document.querySelector("#rate").value;
+    const months = document.querySelector("#months").value;
+    const interest = (amount * (rate * 0.01)) / months;
+    const total = ((amount / months) + interest).toFixed(2);
+
+    document.querySelector("#total").innerHTML = "Monthly PMT:" +total;
+}
